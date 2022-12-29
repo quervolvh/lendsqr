2f3a5543179338350a6d5d767b834d41da955065
Adeyemi Awwal <quervolvh@gmail.com>
2022-12-29 11:12:35 +0100

    src & sass folder set up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.resolve.modules.push(path.resolve('./src'));
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.(png|jpg|gif|svg|eot|otf|ttf|woff|woff2|jpeg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000
+            }
+          }
+        }
+      ]
+    );
+
+    // config.plugins.push(new BundleAnalyzerPlugin())
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
